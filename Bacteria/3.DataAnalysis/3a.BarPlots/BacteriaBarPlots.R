f9037583971d6853fc74e36afae91f55a199d9ee
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2023-05-04 11:54:49 +0300

    Update and rename Bacteria Vine Wood BarPlots.R to BacteriaBarPlots.R

---
+++ Bacteria/3.DataAnalysis/3a.BarPlots/BacteriaBarPlots.R
@@ -0,0 +1,81 @@
+##Bar Plots analysis##
+
+##load phyloseq object for bacteria, (the XXXXXX.RDS File from the construcrion of the phyloseq object raw counts)##
+
+bacteria_vinification_Annotated   
+
+###Merging 4th Stage replicates###
+bacteria_vinification_Annotated.4thStage <- merge_samples(bacteria_vinification_Annotated, "stage_replicate")
+
+bacteria_vinification_Annotated.4thStage <- prune_taxa(taxa_sums(bacteria_vinification_Annotated.4thStage)>0,bacteria_vinification_Annotated.4thStage)
+
+View(data.frame(sample_data(bacteria_vinification_Annotated.4thStage)))
+
+write.table(data.frame(sample_data(bacteria_vinification_Annotated.4thStage)), file="bacteria_vinification_Annotated.4thStage.txt", quote = F,col.names = NA, sep="\t")
+
+SampleDataNew67 <- read.table("bacteria_vinification_Annotated.4thStage.txt", header=T,sep = "\t",row.names = 1)
+
+sample_data(bacteria_vinification_Annotated.4thStage) <- SampleDataNew67
+
+bacteria_vinification_Annotated.4thStage
+
+View(data.frame(sample_data(bacteria_vinification_Annotated.4thStage)))
+
+###RA % Both cultivars
+bacteria_vinification_Annotated.4thStage_100 <- transform_sample_counts(bacteria_vinification_Annotated.4thStage, function(OTU) 100*OTU/sum(OTU))
+
+saveRDS(bacteria_vinification_Annotated.4thStage_100, file = "bacteria_vinification_Annotated.4thStage_100.RDS")
+
+bacteria_vinification_Annotated.4thStage_100
+
+##top 12 ASVs## 
+myTaxa11_bacteria_vinification_Annotated.4thStage_100
+ <- names(sort(taxa_sums(bacteria_vinification_Annotated.4thStage_100
+), decreasing = TRUE)[1:12])  
+
+Top11_bacteria_vinification_Annotated.4thStage_100
+ <- prune_taxa(myTaxa11_bacteria_vinification_Annotated.4thStage_100
+, bacteria_vinification_Annotated.4thStage_100
+)
+
+taxa_names(Top11_bacteria_vinification_Annotated.4thStage_100
+)
+
+- - - - - - - -- - -ASVs plot -  -- - -- - --
+  
+mytax11_bacteria_vinification_Annotated.4thStage_100
+ <- data.frame(tax_table(Top11_bacteria_vinification_Annotated.4thStage_100
+), stringsAsFactors = F)
+
+# For ITS - Remove letter from taxonomy
+for (i in c(1:nrow(mytax11_bacteria_vinification_Annotated.4thStage_100
+))) {
+  for(j in c(1:ncol(mytax11_bacteria_vinification_Annotated.4thStage_100
+))) {
+    mytax11_bacteria_vinification_Annotated.4thStage_100
+[i,j] <- gsub("[a-z]__","",mytax11_bacteria_vinification_Annotated.4thStage_100
+[i,j])
+  }
+}
+
+mytxplot11_bacteria_vinification_Annotated.4thStage_100
+ <- data.frame(OTU = row.names(mytax11_bacteria_vinification_Annotated.4thStage_100
+), 
+                                        txplt = paste(row.names(mytax11_bacteria_vinification_Annotated.4thStage_100
+), " ", mytax11_bacteria_vinification_Annotated.4thStage_100
+$Genus,  ":", mytax11_bacteria_vinification_Annotated.4thStage_100
+$Species,  sep = ""))
+
+row.names(mytxplot11_bacteria_vinification_Annotated.4thStage_100
+) <- mytxplot11_bacteria_vinification_Annotated.4thStage_100
+$OTU
+
+taxa_names(Top11_bacteria_vinification_Annotated.4thStage_100
+) <- mytxplot11_bacteria_vinification_Annotated.4thStage_100
+[taxa_names(Top11_bacteria_vinification_Annotated.4thStage_100
+),"txplt"]
+
+pdf(file = "M.pdf", width = 14, height = 7)
+plot_bar(Top11_bacteria_vinification_Annotated.4thStage_100
+, x="stage", fill="OTU", title = "") + facet_grid(cols = vars(terroir_vinification), rows = vars(year)) + geom_col() + scale_fill_manual(values = mycols)
+dev.off()
